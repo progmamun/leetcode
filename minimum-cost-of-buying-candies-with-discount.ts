70d145b29360161c60ceb069735fd502b68d4aaa
progmamun <mamun333089@gmail.com>
2026-06-01 17:46:58 +0600

    added-candies-discount

---
+++ minimum-cost-of-buying-candies-with-discount.ts
@@ -0,0 +1,12 @@
+function minimumCost(cost: number[]): number {
+  cost.sort((a, b) => b - a);
+
+  let total = 0;
+
+  for (let i = 0; i < cost.length; i++) {
+    if (i % 3 !== 2) {
+      total += cost[i];
+    }
+  }
+  return total;
+}
